5ede6ab9f956fe1dfadf26fb299342de8a571b71
Paul-Bett <bettpaul6@gmail.com>
2022-11-17 16:02:15 +0300

    betty update

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,10 +8,10 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-		if (!node || (node->parent == NULL))
-				return(NULL);
-		if (node->parent->right == node)
-				return(node->parent->left);
-		else
-				return(node->parent->right);
+	if (!node || (node->parent == NULL))
+		return (NULL);
+	if (node->parent->right == node)
+		return (node->parent->left);
+	else
+		return (node->parent->right);
 }
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,10 +8,10 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-		if (!node || (node->parent == NULL) || (node->parent->parent == NULL))
-				return(NULL);
-		if (node->parent->parent->right == node->parent)
-				return(node->parent->parent->left);
-		else
-				return(node->parent->parent->right);
+	if (!node || (node->parent == NULL) || (node->parent->parent == NULL))
+		return (NULL);
+	if (node->parent->parent->right == node->parent)
+		return (node->parent->parent->left);
+	else
+		return (node->parent->parent->right);
 }
